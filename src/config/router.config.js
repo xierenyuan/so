1b506685209a229c8d5ba05a3d9810490e9a7cb5
wangkai <walkerkay@163.com>
2016-07-30 20:18:28 +0800

    add login

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,56 +1,69 @@
 export default router => {
-    //路由配置
-    router.map({
-        '/home': {
-            component(resolve) {
-                require(['../views/home'], resolve);
-            }
+  //路由配置
+  router.map({
+    '/': {
+      component(resolve) {
+        require(['../App'], resolve);
+      },
+      subRoutes: {
+        'home': {
+          component(resolve) {
+            require(['../views/home'], resolve);
+          }
         },
-        '/home/:itemId': {
-            component(resolve) {
-                require(['../views/home/item.vue'], resolve);
-            },
-            name:'homeItem'
+        ':itemId': {
+          component(resolve) {
+            require(['../views/home/item.vue'], resolve);
+          },
+          name: 'homeItem'
         },
-        '/waimai': {
-            component(resolve) {
-                require(['../views/takeOut'], resolve);
-            }
+        'waimai': {
+          component(resolve) {
+            require(['../views/takeOut'], resolve);
+          }
         },
-        '/order': {
-            component(resolve) {
-                require(['../views/order'], resolve);
-            }
+        'order': {
+          component(resolve) {
+            require(['../views/order'], resolve);
+          }
         },
-        '/found': {
-            component(resolve) {
-                require(['../views/found'], resolve);
-            }
+        'found': {
+          component(resolve) {
+            require(['../views/found'], resolve);
+          }
         },
-        '/my': {
-            component(resolve) {
-                require(['../views/my'], resolve);
-            }
+        'my': {
+          component(resolve) {
+            require(['../views/my'], resolve);
+          }
         }
-    });
-    //重定向
-    router.redirect({
-        '*': '/home'
-    });
-    //路由开始时
-    router.beforeEach(({ to, from, next }) => {
-        let toPath = to.path;
-        let fromPath = from.path;
-        if (toPath.replace(/[^/]/g, '').length > 1) {
-            router.app.isApp = false;
-        } else {
-            let depath = toPath === '/' || toPath === '/invite' || toPath === '/rank';
-            router.app.isApp = depath ? 0 : 1;
-        }
-        next();
-    });
-    //路由结束时
-    router.afterEach((transition) => {
-        //console.log('end', transition);
-    });
+      },
+    },
+    '/login': {
+      component(resolve) {
+        require(['../views/user/login'], resolve);
+      }
+    }
+
+  });
+  //重定向
+  router.redirect({
+    '*': '/login'
+  });
+  //路由开始时
+  router.beforeEach(({to, from, next}) => {
+    let toPath = to.path;
+    let fromPath = from.path;
+    if (toPath.replace(/[^/]/g, '').length > 1) {
+      router.app.isApp = false;
+    } else {
+      let depath = toPath === '/' || toPath === '/invite' || toPath === '/rank';
+      router.app.isApp = depath ? 0 : 1;
+    }
+    next();
+  });
+  //路由结束时
+  router.afterEach((transition) => {
+    //console.log('end', transition);
+  });
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
+import Main from './Main.vue';
 import 'ionicons-pre/css/ionicons.min.css';
 import { Toast } from 'mint-ui';
 import RouterConfig from './config/router.config';
@@ -16,6 +17,6 @@ const router = new VueRouter({
 //init router
 RouterConfig(router);
 //启动
-router.start(App, '#app');
+router.start(Main, '#app');
 
 Toast('提示信息');

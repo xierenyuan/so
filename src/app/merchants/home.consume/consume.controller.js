b7609174aad0ce489f7c3d2932a54ce3364c9b56
Xwatson <xuwusheng@hotmail.com>
2016-09-23 15:39:06 +0800

    更新消费模块

--- src/app/merchants/home.consume/consume.controller.js
+++ src/app/merchants/home.consume/consume.controller.js
@@ -1,8 +1,14 @@
 /**
  * Created by xuwus on 2016/9/22.
  */
+import bannerImg from 'assets/images/banner.gif';
+import kksjImg from 'assets/images/home/kksj.png';
+import yqhyImg from 'assets/images/home/yqhy.png';
 export default class ConsumeCtrl {
   constructor() {
+    this.bannerImg=bannerImg;
+    this.kksjImgSrc = kksjImg;
+    this.yqhyImgSrc = yqhyImg;
   }
 
 }
